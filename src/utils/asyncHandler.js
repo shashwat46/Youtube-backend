92329442accad762f92b00b3486c886fcae3d219
shashwat46 <shashwat.singh0406@gmail.com>
2024-12-22 17:37:01 +0530

    chore: setup complete, tested user route with status message

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //MARK: Using promises
 
 const asyncHandler= (requestHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
     }
 }
