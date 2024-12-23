6da6f4a4013c424a20f4a8acce9d6526d9185172
shashwat46 <shashwat.singh0406@gmail.com>
2024-12-23 23:33:02 +0530

    feat: added register controller; refactor: bug fixes in user.model and user.routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,79 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {APIError} from "../utils/APIError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {APIResponse} from "../utils/APIResponse.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "ok"
+    //MARK: STEPS
+    //get user details from frontend
+    //validation - not empty
+    //check if user already exists: username, email
+    //check for images,check for avatar
+    //upload them to cloudinary, check for avatar
+    //Create user object - create entry in DB
+    //remove password and refresh token field from response
+    //check for user creation
+    //return response
+
+
+    const {fullname, email, username, password} = req.body
+    console.log(req.body);
+
+    // if(fullName === ""){
+    //     throw new APIError(400, "fullname is required")   ---> NORMIE WAY TO DO THINGS
+    // }
+
+    if(
+        [fullname, email, username, password].some((field) => field?.trim() === "") // ---> SIGMA WAY TO DO THINGS
+    ){
+        throw new APIError(400, "All fields are required")
+    }
+
+    const existingUser = await User.findOne({
+        $or: [{ username },{ email }]
     })
+    // console.log(existingUser)
+
+    if(existingUser){
+        throw new APIError(409, "User with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new APIError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar) {
+        throw new APIError(400, "Avatar file is required")
+    }
+
+   const user = await User.create({
+        fullname, 
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username : username.toLowerCase()
+    })
+
+    const userIsCreated = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser) {
+        throw new APIError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new APIResponse(200, createdUser, "User registered Succesfully")
+    )
+
 })
 
 
--- src/utils/APIResponse.js
+++ src/utils/APIResponse.js
@@ -6,3 +6,5 @@ class APIResponse{
         this.sucess = statusCode < 400
     }
 }
+
+export {APIResponse}
